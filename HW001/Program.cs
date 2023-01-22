b55e5ab645f70ec2c0109e4ecef03fb3785c6c5d
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-22 10:17:44 +0500

    perhaps a working program has been created

---
+++ HW001/Program.cs
@@ -0,0 +1,9 @@
+﻿//Найти кубы чисел от 1 до N
+int n = new Random().Next(1, 10);
+Console.WriteLine(n);
+for (int i = 1; i<=n; i++)
+{
+    Console.Write(i + " - ");
+    int result = i * i * i;
+    Console.WriteLine(result);
+}
