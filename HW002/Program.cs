25ba433f12cffef5ce997f644416512c19185b25
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-22 10:22:08 +0500

    perhaps a working program has been created

---
+++ HW002/Program.cs
@@ -0,0 +1,12 @@
+﻿using Internal;
+using System;
+// Найти сумму чисел от 1 до А
+int n = new Random().Next(10, 100);
+int sum = 0;
+Console.WriteLine("Сумма числел от 1 до " + n);
+
+for (int i = 1; i <= n; i++)
+{
+    sum = sum + i;
+}
+Console.Write(sum);
