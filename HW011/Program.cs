c1366dab5cb55f43f666dead8e1b630817a9716b
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-28 09:27:58 +0500

    perhaps a working program has been created

---
+++ HW011/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] array = new int[11];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(10, 100);
+        if (i == 0 || i%2==0)
+        {
+            sum = sum + array[i];
+            Console.WriteLine (sum);
+        }
+}
+
