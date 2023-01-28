54b1ba87f11b7efe218b8ccc19a4f504969e6df6
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-28 16:04:58 +0500

    perhaps a working program has been created

--- HW012/Program.cs
+++ HW012/Program.cs
@@ -1,15 +1,10 @@
 ﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 int[] array = {1,2,3,4,5,6,7,8,9,10};
-int count = 0;
 
-  foreach (var item in array)
-  {
-   int n1 = item;
-   int n2 = item;
-   int diff = n1*n2;
-Console.WriteLine(diff);
-  }
- 
-
-
-   
+for (int i = 0; i < array.Length; i++)
+{
+    int n1 = array[i];
+    int n2 = array[array.Length - 1 - i];
+    int diff = n1*n2;
+    Console.WriteLine(diff);
+}
