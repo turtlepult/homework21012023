04c62b9cf90e365cf2c84d4b125fdc6e6d2923ed
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-28 14:44:42 +0500

    perhaps a working program has been created

--- HW006/Program.cs
+++ HW006/Program.cs
@@ -4,7 +4,7 @@ for (int i = 1; i<=n; i++)
 {
     int result = i * i * i;
     
-        if (result%2==0)
+        if (result%2==0) 
         {
             Console.WriteLine(result);
         }
---
+++ HW009/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+int[] array = new int[10];
+int countPos = 0;
+    int countNeg = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 200);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    
+    if (array[i]%2==0)
+    {
+        countPos++;
+    }
+    else
+        countNeg++;
+
+}
+Console.Write("+ " + countPos + ", - "+ countNeg);
