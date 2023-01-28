c88208790228d68e56fde7bf630a46f09a002125
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-28 14:33:52 +0500

    perhaps a working program has been created

---
+++ HW003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+int a = 3;
+int b = 5;
+int res = 1;
+for (int i = 0; i < b; i++)
+{
+    
+    res = res * a;
+    Console.Write(res + " ");
+}
