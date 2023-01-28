03f89737b22c3864a0c17ef19418cafeb45c0a10
Ivan_Ladygin <ivan_ladiygin@mail.ru>
2023-01-28 14:38:09 +0500

    perhaps a working program has been created

---
+++ HW006/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+int n = new Random().Next(1, 10);
+for (int i = 1; i<=n; i++)
+{
+    int result = i * i * i;
+    
+        if (result%2==0)
+        {
+            Console.WriteLine(result);
+        }
+}
